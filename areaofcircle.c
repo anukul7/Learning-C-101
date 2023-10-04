ba9bb7db7fd33aa5f63edff88b3f8c28393b0107
anukul7 <anushkakulkarni07052007@gmail.com>
2023-10-04 19:06:44 +0530

    task3(as expected)

--- areaofcircle.c
+++ areaofcircle.c
@@ -4,6 +4,8 @@ int main()
     float radius;
     printf("enter radius");
     scanf("%f", &radius);
+    const double pi = 3.14;
+    printf("the value of pi is : %fl\n" , pi);
     printf("area is : %f", 3.14 * radius * radius);
     return 0;
 }
---
+++ asmd.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{ 
+    printf("sum,div,multiply,subract A and B");
+    int a = 4, b = 16;
+    int sum = a + b;
+    int multiply = a * b;
+    int divide = b / a;
+    int subtract = b - a;
+    return 0;
+}
