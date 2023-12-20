be9daa7ea75aa7e9b47ce09b10111fce25d51d5f
anukul7 <anushkakulkarni07052007@gmail.com>
2023-12-20 21:00:44 +0530

    perimeterofsq

---
+++ Assignment-01/perimetersq.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+// calculating perimeter of a square
+#define MY_CONSTANT 4
+    float length;
+    printf("enter length");
+    scanf("%f", &length);
+    printf("perimeter is : %f", 4 * length);
+    return 0;
+}
