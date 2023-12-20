e0058d25c1138755237d70a45a130bf325e180a5
anukul7 <anushkakulkarni07052007@gmail.com>
2023-12-20 22:24:37 +0530

    area of circle 2

---
+++ Assignment-01/circlearea2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    const float pi = 3.14;
+    int radius = 7;
+    int multiply = pi * radius * radius;
+    printf("area is : %d", multiply);
+
+    return 0;
+}
